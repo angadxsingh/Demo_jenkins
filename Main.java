1380eba542ff5eaed63b0eff0331cf15723f3b45
angadxsingh <106704008+angadxsingh@users.noreply.github.com>
2024-03-01 09:45:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@
 public class Main {
     public static void main(String[] args) {
     System.out.println("Thisss is a test for DevOps");
-     System.out.println("Ssend mmaaail and slack notification!")
+     System.out.println("Ssend mmaaail and slack notification!");
     }
 }
