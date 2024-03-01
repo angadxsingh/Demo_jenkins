afb186085a6b3ec00211f21c226fa1f27fcb99f2
angadxsingh <106704008+angadxsingh@users.noreply.github.com>
2024-03-01 20:39:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  
 public class Main {
     public static void main(String[] args) {
-    System.out.println("This file is a test for DevOps");
+    System.out.println("This file is a test file for DevOps");
      System.out.println("On making changes it should end mail and slack notification!");
     }
 }
